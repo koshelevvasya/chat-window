a9a00cf644f19cec1db8a73b1645f705b2e6a6c5
Vasiliy Koshelev <vasiliy.koshelev@iqtek.ru>
2020-08-12 01:19:49 +0300

    Поправлен url ws на рабочий

--- public/config.js
+++ public/config.js
@@ -1,6 +1,6 @@
 function getAppConfig () {
   return {
-    webSocketsUrl: "ws://pwerm.tada.team/ws?name={NAME}"
+    webSocketsUrl: "ws://pm.tada.team/ws?name={NAME}"
     // webSocketsUrl: "{THIS_WS_PROTOCOL}//{THIS_HOST}/ws?name={NAME}"
   }
 }
